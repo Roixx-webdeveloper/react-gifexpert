70e9157a128a8d66175e37f3ff37b9b523c99bd9
Oscar Jimenez- Web Developer <53940450+Roixx-webdeveloper@users.noreply.github.com>
2021-04-23 17:20:36 -0400

    First Commit

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,15 @@
+export const getGifs = async (category) => {
+    const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=H4m7K4I6oAC4Nn686IADrl4iU2MTWjYz`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifs = data.map(img => {
+        return {
+            id: img.id,
+            title: img.title,
+            url: img.images?.downsized_medium.url
+        }
+    })
+    return gifs;
+
+}
